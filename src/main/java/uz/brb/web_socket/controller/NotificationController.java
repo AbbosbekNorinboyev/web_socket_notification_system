a2fdc3dbdacf959a0480e6adad5d4f72c7a4bf70
AbbosbekNorinboyev <norinboyevabboskhan002@gmail.com>
2025-09-26 11:50:07 +0500

    web sockete notification system

---
+++ src/main/java/uz/brb/web_socket/controller/NotificationController.java
@@ -0,0 +1,15 @@
+package uz.brb.web_socket.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NotificationController {
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/notification")
+    public String sendMessage(String message) {
+        System.out.println("message = " + message);
+        return message;
+    }
+}
